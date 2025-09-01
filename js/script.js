c92f9dc0ac24aa7949237e33af36bfb1f6fbf946
OLEKSANDR VEREMIIENKO <120383111+SkyKeyUA@users.noreply.github.com>
2025-09-01 04:03:24 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', windowLoad);
+const header = document.querySelector('.header');
+const html = document.documentElement;
+const bannerPosts = document.querySelector('.posts__banner');
+
+const heightHeader = (tag) => {
+  tag.style.top = `${header.offsetHeight}px`;
+};
+bannerPosts && heightHeader(bannerPosts);
+
+function windowLoad() {
+  if (992 >= window.innerWidth) {
+    document.addEventListener('click', documentActions);
+  }
+  html.classList.add('loaded');
+}
+function documentActions(e) {
+  const targetElement = e.target;
+
+  if (targetElement.closest('.icon-menu')) {
+    html.classList.toggle('menu-open');
+  }
+  targetElement.closest('.menu__link') && html.classList.contains('menu-open')
+    ? html.classList.remove('menu-open')
+    : null;
+}
